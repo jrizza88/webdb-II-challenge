0a47515fbf6839b0824d0e1be0377b3d467484f3
jrizza88 <jetorres88@gmail.com>
2019-03-15 21:29:32 -0400

    successful post and get request

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
 
 const express = require('express');
 const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('../knexfile');
 
-
-const db = knex(knexConfig.development);
-
-const router = require('../zoo/zooRouter');
+const zooRouter = require('../zoo/zooRouter');
 
 
 const server = express();
@@ -15,6 +10,6 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-server.use('api/zoos', zooRouter);
+server.use('/api/zoos', zooRouter);
 
 module.exports = server;
--- zoo/zooRouter.js
+++ zoo/zooRouter.js
@@ -4,8 +4,31 @@ const router = require('express').Router();
 const knex = require('knex');
 const knexConfig = require('../knexfile');
 
-
 const db = knex(knexConfig.development);
+
+router.get('/', async (req, res) => {
+    try {
+        const zoo = await db('zoos');
+        res.status(200).json(zoo);
+    } catch (error){
+        res.status(500).json({error: 'error with get request'})
+    }
+});
+
+router.post('/', async (req, res) => {
+   try{
+        
+        const [id] = await db('zoos').insert(req.body);
+        const zoo = await db('zoos')
+        .where({ id })
+        .first();
+        res.status(201).json(zoo);
+
+   } catch (error) {
+    const message = errors[error.errno] || "We ran into a post error";
+        res.status(500).json({message, error})
+   }
+});
 
 module.exports = router;
 
