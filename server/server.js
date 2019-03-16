3ba10cd71a3ae5a2fa7b85d267b9e86dc4fced28
jrizza88 <jetorres88@gmail.com>
2019-03-15 20:54:39 -0400

    fixed configuration of files and corrected exports and routing of files

--- server/server.js
+++ server/server.js
@@ -7,10 +7,14 @@ const knexConfig = require('../knexfile');
 
 const db = knex(knexConfig.development);
 
+const router = require('../zoo/zooRouter');
+
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use('api/zoos', zooRouter);
 
 module.exports = server;
--- zoo/zooRouter.js
+++ zoo/zooRouter.js
@@ -1,6 +1,12 @@
 
 const router = require('express').Router();
 
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+
+const db = knex(knexConfig.development);
+
 module.exports = router;
 
 		// "start": "nodemon index.js"
